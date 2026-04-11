22e2b268ae626ba6d19f6214a011637c979f7065
Taarun <taarun96@gmail.com>
2026-04-11 12:32:58 +0530

    used arrow function to enhance code readability

--- src/test/java/com/api/utils/ConfigManager.java
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -5,53 +5,44 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigManager {
-	
-	
+
 	private static Properties prop = new Properties();
-	private static String path="config/config.properties";
-	
+	private static String path = "config/config.properties";
+
 	private ConfigManager() {
-		
+
 	}
+
 	static {
-	
-		String env=System.getProperty("env","qa");
-		env=env.toLowerCase().trim();
-		System.out.println("Running tests in environment: "+env);
-		
+
+		String env = System.getProperty("env", "qa");
+		env = env.toLowerCase().trim();
+		System.out.println("Running tests in environment: " + env);
+
 		switch (env) {
-		case "qa":
-			path="config/config.qa.properties";
-			break;
-		case "dev":
-			path="config/config.dev.properties";
-			break;
-		case "uat":
-			path="config/config.uat.properties";
-			break;
-		default:
-			path="config/config.qa.properties";
+		case "qa" -> path = "config/config.qa.properties";
+		case "dev" -> path = "config/config.dev.properties";
+
+		case "uat" -> path = "config/config.uat.properties";
+
+		default -> path = "config/config.qa.properties";
 		}
-	
-		InputStream input=Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-		if(input==null) {
-			throw new RuntimeException("Unable to find config.properties file in classpath"+path);
+
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+		if (input == null) {
+			throw new RuntimeException("Unable to find config.properties file in classpath" + path);
 		}
-		
+
 		try {
 			prop.load(input);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	
-		
-		
-}
-	
-	
-		public static String getProperties(String key) {
+
+	}
+
+	public static String getProperties(String key) {
 		return prop.getProperty(key);
-		}
+	}
 }
