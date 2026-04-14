cd8a48d35b5a5b56f01b050f274fb271159af4ac
Taarun <taarun96@gmail.com>
2026-04-14 21:01:09 +0530

    Added CreateJobAPIDataDrivenTest

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -1,16 +1,13 @@
 package com.api.utils;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.List;
 
-import com.dataproviders.api.bean.UserBean;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvException;
 
 public class CSVReaderUtil {
 	
@@ -19,26 +16,20 @@ public class CSVReaderUtil {
 		
 	}
 	
-	public static Iterator<UserBean> loadCSV(String pathOfCSVFile) throws IOException, CsvException {
-		
-		
+	public static <T> Iterator<T> loadCSV(String pathOfCSVFile, Class<T> bean) {
 
-		
-		InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
+	    // Using Thread.currentThread() to get the resource from the classpath
+	    InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
+	    InputStreamReader isr = new InputStreamReader(is);
+	    CSVReader csvReader = new CSVReader(isr);
 
-        InputStreamReader isr=new InputStreamReader(is);	
-        
-        CSVReader csvReader=new CSVReader(isr);
-        
-        
-        CsvToBean<UserBean> csvToBean =new CsvToBeanBuilder(csvReader)
-        		.withType(UserBean.class)
-        		.withIgnoreEmptyLine(true)
-        		.build();
-        
-        
-        List<UserBean> userList=csvToBean.parse();
-        return userList.iterator();
-        
-}
+	    // Using CsvToBeanBuilder to map the CSV columns to the Java Bean
+	    CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(csvReader)
+	            .withType(bean)
+	            .withIgnoreEmptyLine(true)
+	            .build();
+
+	    List<T> list = csvToBean.parse();
+	    return list.iterator();
+	}
 }
